d1698c967a41610c2893e097d14997f1d368b06c
André Torres <andretorresjava@gmail.com>
2023-11-21 13:00:38 -0500

    Un apunte sobre la recursividad.

--- Module04/file-system.js
+++ Module04/file-system.js
@@ -17,3 +17,18 @@ async function contentTypeDistinction() {
   }
 }
 contentTypeDistinction();
+
+// Un apunte sobre recursividad
+async function findFiles(folderName) {
+  const items = await fs.readdir(folderName, { withFileTypes: true });
+  items.forEach((item) => {
+    if (item.isDirectory()) {
+      // this is a folder, so call this method again and pass in
+      // the path to the folder
+      findFiles(`${folderName}/${item.name}`);
+    } else {
+      console.log(`Found file: ${item.name} in folder ${folderName}`);
+    }
+  });
+}
+findFiles("stores");
