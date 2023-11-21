06af0dc5a0c458537c3f459c0e4f7f249c97fa30
André Torres <andretorresjava@gmail.com>
2023-11-20 20:58:59 -0500

    Trabajo con el sistema de archivos.

--- Module04/file-system.js
+++ Module04/file-system.js
@@ -1,1 +1,19 @@
 const fs = require("fs").promises;
+
+// Muestra del contenido en un directorio
+async function showContentDirectory() {
+  const items = await fs.readdir("stores");
+  console.log(items);
+}
+showContentDirectory();
+
+// Distinción del tipo de contenido
+async function contentTypeDistinction() {
+  const items = await fs.readdir("stores", { withFileTypes: true });
+  console.log(items);
+  for (let item of items) {
+    const type = item.isDirectory() ? "folder" : "file";
+    console.log(`${item.name}: ${type}`);
+  }
+}
+contentTypeDistinction();
