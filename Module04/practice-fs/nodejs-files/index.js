771694ff5a25a22105fb4642db76bcfa559eed89
André Torres <andretorresjava@gmail.com>
2023-12-06 15:45:51 -0500

    Ejercicio: Trabajar con rutas.

--- Module04/practice-fs/nodejs-files/index.js
+++ Module04/practice-fs/nodejs-files/index.js
@@ -1,16 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-async function main() {
-  const salesDir = path.join(__dirname, "stores");
-
-  const salesFiles = await findSalesFiles(salesDir);
-  console.log(salesFiles);
-}
-
-main();
-
-// Escritura de un método para buscar los archivos sales.json
 async function findSalesFiles(folderName) {
   // this array will hold sales files as they are found
   let salesFiles = [];
@@ -19,20 +9,33 @@ async function findSalesFiles(folderName) {
     // read all the items in the current folder
     const items = await fs.readdir(folderName, { withFileTypes: true });
 
-    // iterable over each found item
+    // iterate over each found item
     for (item of items) {
+      // if the item is a directory, it will need to be searched
       if (item.isDirectory()) {
-        // search this directory for files (this is recursion!)
-        await findFiles(`${folderName}/${item.name}`);
+        // call this method recursively, appending the folder name to make a new path
+        await findFiles(path.join(folderName, item.name));
       } else {
-        //Make sure the discovered file is a sales.json file
-        if (item.name === "sales.json") {
+        // Make sure the discovered file is a .json file
+        if (path.extname(item.name) === ".json") {
           // store the file path in the salesFiles array
-          await salesFiles.push(`${folderName}/${item.name}`);
+          await salesFiles.push(path.join(folderName, item.name));
         }
       }
     }
   }
+
   await findFiles(folderName);
+
   return salesFiles;
 }
+
+async function main() {
+  const salesDir = path.join(__dirname, "stores");
+
+  // find paths to all the sales files
+  const salesFiles = await findSalesFiles(salesDir);
+  console.log(salesFiles);
+}
+
+main();
