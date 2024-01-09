01954b8680840436d6ef74eb2458b9c809d95be3
André Torres <andretorresjava@gmail.com>
2024-01-09 15:57:18 -0500

    Servicio de tipos de contenido diferentes.

--- Module05/example-web-application/index.js
+++ Module05/example-web-application/index.js
@@ -6,11 +6,14 @@ const server = http.createServer((req, res) => {
     console.log('You received a chunk of data', chunk);
   });
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // res.end('hello world');
-  res.end('some data');
+  // res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // // res.end('hello world');
+  // res.end('some data');
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ id: 1, name: 'Catcher in the Rye' }));
 });
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
-})
+});
