ad74284cdfce284c3fc8df3a58c0e21bbad9c47f
André Torres <andretorresjava@gmail.com>
2024-01-18 23:54:58 -0500

    Creación de una aplicación web básica con Express.

--- Module05/example-web-application/index.js
+++ Module05/example-web-application/index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   });
 
   // res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // // res.end('hello world');
+  // res.end('hello world');
   // res.end('some data');
 
   res.writeHead(200, { 'Content-Type': 'application/json' });
